10c1570ad107d6dea9d697dfb4efe4a368eebd61
Mateo Nossa <jmnossah@academia.usbbog.edu.co>
2023-02-28 14:54:39 -0500

    Nuevo archivo

---
+++ ClaseTeoria/Clase28.02.py
@@ -0,0 +1,71 @@
+#Ciclos: While
+n=0
+while n<10:
+    n+=1
+    print(n)
+    if n>6:
+        #break
+        continue
+print("Fin del proceoso") 
+
+
+
+
+
+
+
+
+
+##PUNTO1
+#edad=int(input("Ingrese su edad: "))
+#if edad>=18:
+#    print("Usted es mayor de edad. ")
+#else: 
+#    print("usted es menor de edad.")
+#print("Fin del programa")
+##PUNTO2: 
+#print("Funcion cuadratica por formula")
+#a=float(input("Ingrese el valor de a:"))
+#b=float(input("Ingrese el valor de b:"))
+#c=float(input("Ingrese el valor de c: "))
+#r=b**2-4*a*c
+#if r<0:
+#    print("Su respuesta es imaginaria")
+#elif r==0:
+#    d=-b/(2*a)
+#    print("La solucion es ",d)
+#else:
+#    d1=(-b+(r**(1/2)))/(2*a)
+#    d2=(-b-(r**(1/2)))/(2*a)
+#    print("las suluciones son: ",d1," y ",d2)
+#
+
+#day=input("Ingrese el dia= ")
+#if day== "Lunes": 
+#    print("Es el primer dia de la semana")
+#elif day == "Martes": 
+#    print("Es el segundo dia de la semana")
+#elif day == "Miercules": 
+#    print("Es el tercero dia de la semana")
+#elif day == "Jueves": 
+#    print("Es el cuarto dia de la semana")
+#elif day == "viernes": 
+#    print("Es el quinto dia de la semana")
+#elif day == "Sabado": 
+#    print("Es el sexto dia de la semana")
+#elif day == "Domingo": 
+#    print("Es el septimo dia de la semana")    
+#else: 
+    #print("La entrada no es valida")
+
+
+#num = int(input("Ingrese un numero: "))elif day == "Lunes": 
+
+#if num<10:
+    #print("el numero es menor que 10.")
+#elif num==10:
+    #print("El numero es igual a 10. ")
+#else: 
+    #print("El numero es mayor que 10.")
+
+#print("Fin del proceso")
